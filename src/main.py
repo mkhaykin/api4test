9cd8f44a0697a5a1a0f1cc3ecb79303bbecb493b
Mikhail Khaykin <m.khaykin@outlook.com>
2025-01-31 08:40:32 +0300

    feat: add endpoints

--- src/main.py
+++ src/main.py
@@ -14,5 +14,14 @@ async def echo(msg: str) -> dict:
     return {"msg": f"{msg}"}
 
 
+@app.post("/callback")
+async def callback() -> dict:
+    return {"callback": "Hello World"}
+
+
+@app.get("/healthcheck")
+async def healthcheck() -> dict:
+    return {"healthcheck": "Hello World"}
+
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000)
